978e2e4857db0429447514eebe3583c9f9541a2a
mandel <nelsonokiki@gmail.com>
2025-06-17 00:46:07 +0100

    update

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     #     from waitress import serve
     #     serve(app, host="0.0.0.0", port=5000)
     # else:
-        app.run(debug=True)
+        app.run(debug=False)
