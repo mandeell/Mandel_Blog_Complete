cba3a34e07e4f887a51c4ca01152e947bb964ea5
mandel <nelsonokiki@gmail.com>
2025-06-16 16:26:25 +0100

    updated the database to accommodate author id and create a relationship between the user and blogposts

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Text, Boolean
 
 
@@ -11,15 +11,18 @@ db = SQLAlchemy(model_class=Base)
 
 # CONFIGURE TABLE
 class BlogPost(db.Model):
+    __tablename__ = 'blog_posts'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('users.id'))
+    author =     relationship('User', back_populates='posts')
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
     date: Mapped[str] = mapped_column(String(250), nullable=False)
     body: Mapped[str] = mapped_column(Text, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(200))
@@ -27,5 +30,4 @@ class User(UserMixin, db.Model):
     phone: Mapped[str] = mapped_column(String())
     agent: Mapped[bool] = mapped_column(Boolean())
     admin: Mapped[bool] = mapped_column(Boolean())
-    # reset_token: Mapped[str] = mapped_column(String(200), nullable=True)
-    # reset_token_expiration: Mapped[db.DateTime] = mapped_column(db.DateTime, nullable=True)
+    posts = relationship('BlogPost', back_populates='author')
