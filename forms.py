0f207c74d312aafc1889721f29ceb0d5a154e952
mandel <nelsonokiki@gmail.com>
2025-06-16 16:35:52 +0100

    removed the field author so that the database will automatically link the posts to the current user

--- forms.py
+++ forms.py
@@ -21,7 +21,6 @@ def validate_phone(form, field):
 class CreatePostForm(FlaskForm):
     title = StringField('Blog Post Title', validators=[DataRequired()])
     subtitle = StringField('Subtitle', validators=[DataRequired()])
-    author = StringField('Your Name', validators=[DataRequired()])
     img_url = StringField('Blog Image URL', validators=[DataRequired(), URL()])
     body = CKEditorField('Blog Content', validators=[DataRequired()])
     submit = SubmitField('Submit Post')
